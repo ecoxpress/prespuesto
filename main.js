b3fd4abc6eb441c7922cd02be67489101d19d699
Claudio Gomez <45300398+ecoxpress@users.noreply.github.com>
2023-10-13 19:04:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ function calculate() {
 }
 
 var newRow =
-  '<tr><td><a class="control removeRow" href="#">x</a><span contenteditable>00001</span></td><td><span contenteditable>Gorro de Egresado confeccionado en tela jersey <br>Borla hilo de seda  <br>Nombre impreso en el frente + Promo 23</span></td><td class="amount"><input type="text" value="1"/></td><td class="rate"><input type="text" value="1600" /></td><td class="tax taxrelated"></td><td class="sum"></td></tr>';
+  '<tr><td><a class="control removeRow" href="#">x</a><span contenteditable>00001</span></td><td><span contenteditable>Gorro de Egresado confeccionado en tela jersey <br>Borla hilo de seda  <br>Nombre impreso en el frente + Promo 23</span></td><td class="amount"><input type="text" value="1"/></td><td class="rate"><input type="text" value="1700" /></td><td class="tax taxrelated"></td><td class="sum"></td></tr>';
 
 $(".invoicelist-body").on("keyup", "input", function () {
   calculate();
